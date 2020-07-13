6591e2d14358f42477147bab7ac886eb2f011bcd
Giovanni Ficarra <giovannificarra95@gmail.com>
2020-07-13 18:36:32 +0200

    login 2 (minor gui improvement)

--- core/login.py
+++ core/login.py
@@ -13,15 +13,14 @@ class LoginBox(Gtk.Box):
         self.infoLabel = infoLabel
         self.window = window
 
-        username_label = Gtk.Label("Username")
-        mainBox.pack_start(username_label, True, True, 0)
+        self.username_label = Gtk.Label("Username")
+        mainBox.pack_start(self.username_label, True, True, 0)
         self.username_entry = Gtk.Entry()
         self.username_entry.set_placeholder_text("Insert your username")
         mainBox.pack_start(self.username_entry, True, True, 0)
 
-
-        password_label = Gtk.Label("Password")
-        mainBox.pack_start(password_label, True, True, 0)
+        self.password_label = Gtk.Label("Password")
+        mainBox.pack_start(self.password_label, True, True, 0)
         self.password_entry = Gtk.Entry()
         self.password_entry.set_visibility(False)
         self.password_entry.set_placeholder_text("Insert your password")
@@ -29,23 +28,28 @@ class LoginBox(Gtk.Box):
 
         self.submit_button = Gtk.Button.new_with_label("Submit")
         self.submit_button.connect('clicked', self.submit)
-        self.submit_button.set_receives_default(True)
         mainBox.pack_start(self.submit_button, True, True, 0)
-        window.set_receives_default(self.submit_button)
 
     def submit(self, button):
         query = f"SELECT * FROM users WHERE username='{self.username_entry.get_text()}' AND psw='{self.password_entry.get_text()}';"
         users = util.execute_query(query).fetchall()
 
         if len(users) == 1:
-
             self.window.youtube.show_button(self.window)
+            self.hide()
         else:
             self.infoLabel.set_text("Wrong credentials.")
 
     def show(self, window):
         window.show_all()
         self.username_entry.grab_focus()
+    
+    def hide(self):
+        self.username_label.hide()
+        self.username_entry.hide()
+        self.password_label.hide()
+        self.password_entry.hide()
+        self.submit_button.hide()
 
     def keyPressed(self, widget, event, data=None):
         key = event.keyval
